ab3f6e7cf37be0a79544312ebfd310e982f07959
kelvin <fishaputrakelvin1810@gmail.com>
2021-10-17 21:25:08 +0800

    try-newcode

--- signupcheck.php
+++ signupcheck.php
@@ -1,5 +1,15 @@
 <?php
 
+try
+{
+
+  $dsn = 'mysql:dbname=cpsvaccine;host=localhost;charset=utf8';
+  $db_user = "root";
+  $db_password = "";
+
+  $dbh = new PDO($dsn, $db_user, $db_password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
   $username = $_POST['username'];
   $password =  $_POST['password'];
   $email = $_POST['email'];
@@ -7,36 +17,53 @@
   $ICPassport = $_POST['ICPassport'];
 
 
-// 1. DB Server connection
-  $con = mysqli_connect('localhost', 'root', '', 'cpsvaccine');
-// check the connection
-if($con->connect_error)
-    die(" DB Connection failed " . $con->connect_error);
+  $username = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
+  $password = htmlspecialchars($password, ENT_QUOTES, 'UTF-8');
+  $email = htmlspecialchars($nemail, ENT_QUOTES, 'UTF-8');
+  $fullName = htmlspecialchars($fullName, ENT_QUOTES, 'UTF-8');
+  $ICPassport = htmlspecialchars($ICPassport, ENT_QUOTES, 'UTF-8');
 
-//Check existing user
-$query ="SELECT * FROM tb_patients WHERE username='$username';";
-$result = mysqli_query($con,$query);
- if(mysqli_num_rows($result)>0){
-     echo '<script>alert("Userename already exist.");window.location = "signupPatient.php";</script>';
+  $sql_checkusername = 'SELECT username FROM tb_patients WHERE username=?';
+  $prepare = $dbh->prepare($sql_checkusername);
+  $prepare->bindValue(1, $username, PDO::PARAM_STR);
+  $prepare->execute();
 
-}else{
-  // Insert the values into database table users
-  $sqlQuery = "INSERT INTO tb_patients VALUES ('$username', '$password', '$email', '$fullName', '$ICPassport')";
+  $result = $prepare->fetch(PDO::FETCH_ASSOC);
 
-  // Execute the query
-  if ($con->query($sqlQuery) == TRUE ) {
-    echo '<script>alert("Sign up successful");window.location = "index.php";</script>';
+  if($result==false)
+  {
+    $sql = 'INSERT INTO tb_volunteers(username, password, email, fullName, ICPassport) VALUES (?, ?, ?, ?, ? )';
+    $prepare = $dbh->prepare($sql)
+    $prepare->bindValue(1, $username, PDO::PARAM_STR);
+    $prepare->bindValue(2, $password, PDO::PARAM_STR);
+    $prepare->bindValue(3, $email, PDO::PARAM_STR);
+    $prepare->bindValue(4, $fullName, PDO::PARAM_STR);
+    $prepare->bindValue(5, $ICPassport, PDO::PARAM_STR);
+
+    $prepare->execute();
+
+    $dbh = null;
+
+    session_start();
+    $_SESSION['p_login'] = 1;
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+    $_SESSION['email'] = $email;
+    $_SESSION['fulName'] = $fullName;
+    $_SESSION['ICPassport'] = $ICPassport;
+    echo '<script>alert("Sign Up successfully")';
+    header('Location: index.php');
+    exit();
   }
-  else {
-    echo "<script>alert('sign up failed');</script>";
+  else
+  {
+    echo '<script>alert("This Username is already used");window.location = "signupPatient.php";</script>';
   }
 }
-
-
-
-
-
-// Close DB connection
-$con->close();
-
+catch (Exception $e)
+{
+  $error = $e->getMessage();
+  print "{$error}";
+  exit();
+}
 ?>
