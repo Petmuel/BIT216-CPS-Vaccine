ca2480ae4f66a48d73ad27b164aadf667bf84a51
kelvin <fishaputrakelvin1810@gmail.com>
2021-10-17 16:30:30 +0800

    testing

--- signupAdmin.php
+++ signupAdmin.php
@@ -57,7 +57,7 @@
     <div class="container margin-top">
       <h2>Sign up</h2>
 
-      <form method="post" class="form" name="signup" action="signupcheck.php">
+      <form method="post" class="form" name="signup" action="signupcheck.php" method="POST">
         <div class="form-group mt-3">
           <label for="username">Username</label>
           <input type="text" name="username" class="form-control" id="username" aria-describedby="username"
