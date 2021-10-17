1941c0b3fbb1c57f646023c62711f6467e09c83c
kelvin <fishaputrakelvin1810@gmail.com>
2021-10-17 18:27:58 +0800

    testing

--- signupcheck.php
+++ signupcheck.php
@@ -32,6 +32,27 @@ $result = mysqli_query($con,$query);
   }
 }
 
+
+function signupcheck() {
+  var username = document.signup.username.value;
+  var password = document.signup.password.value;
+  var name = document.signup.name.value;
+
+  if (username == null || username == '') {
+    alert("userName can't be blank");
+    return;
+  } else if (password.length < 6) {
+    alert('Password must be at least 6 characters long');
+    return;
+  } else if (name == null || name == '') {
+    alert("userName can't be blank");
+    return;
+  } else {
+    return document.signup.submit();
+  }
+}
+
+
 // Close DB connection
 $con->close();
 
