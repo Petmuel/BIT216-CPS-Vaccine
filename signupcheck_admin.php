1382bd7950e7e88c905303eb07f563243dfc1dfa
Petmuel <49142438+Petmuel@users.noreply.github.com>
2021-10-18 08:57:44 +0800

    f

--- signupcheck_admin.php
+++ signupcheck_admin.php
@@ -6,14 +6,14 @@ if (isset($_POST['submit'])){
     $username = $_POST['username'];
     $password =  $_POST['password'];
     $email = $_POST['email'];
-    $fullName = $_POST['fullName'];
+    $fullname = $_POST['fullname'];
 
   
     
 
     //Check existing user
     $sql ="insert into tb_admins (username, password, email, fullName) 
-          values('$username', '$password', '$email', '$fullName');";
+          values('$username', '$password', '$email', '$fullname');";
     mysqli_query($conn, $sql);
     echo '<script>alert("Sign up successful");window.location = "index.php";</script>';
  
