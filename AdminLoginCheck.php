66fc1bb4d1f912ad19d4c63ae68cdfb316ae1791
Petmuel <49142438+Petmuel@users.noreply.github.com>
2021-11-14 17:29:34 +0800

    l

--- AdminLoginCheck.php
+++ AdminLoginCheck.php
@@ -9,18 +9,24 @@
     //Check existing user
     $sql ="select * from tb_admins WHERE username='$username';";
     $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
+    
 
     //check if the admin has clicked the 'Record' button or not
     if (isset($_POST['submit'])){
-
-        //validate login input
-        if($row['username'] == $username && $row['password'] == $password){
-            $staffID = $row['staffID'];
-            $_SESSION["user_name"] = $username;
-            $_SESSION["staff_ID"] = $staffID;
-            echo '<script>alert("Login successful!");window.location.href = "recordNewVaccineBatch.php";</script>';
-            exit();
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            //validate login input
+            if($row['username'] == $username && $row['password'] == $password){
+                $staffID = $row['staffID'];
+                $_SESSION["user_name"] = $username;
+                $_SESSION["staff_ID"] = $staffID;
+                echo '<script>alert("Login successful!");window.location.href = "recordNewVaccineBatch.php";</script>';
+                exit();
+            }
+            else{
+                echo '<script>alert("Username or password does not match");window.location.href = "signinAdmin.php";</script>';
+                exit();
+            }
         }
         else{
             echo '<script>alert("Username or password does not match");window.location.href = "signinAdmin.php";</script>';
