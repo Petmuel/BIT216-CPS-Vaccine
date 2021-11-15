c0524de7076d478e2f68ac8a2612d8da75426bf2
Petmuel <49142438+Petmuel@users.noreply.github.com>
2021-11-15 22:29:44 +0800

    .

--- confirm_recordVaccinations.php
+++ confirm_recordVaccinations.php
@@ -4,10 +4,23 @@
     if (isset($_GET['confirm'])) {
         //get vaccinationID from current vaccination
         $vaccinationID = $_GET['vID'];                     
-        //Check existing user
+
         $sql = "UPDATE tb_vaccinations SET status='In Progress' where vaccinationID='$vaccinationID';";
         $result = mysqli_query($conn,$sql);
 
+        $sql ="SELECT * FROM tb_vaccinations WHERE vaccinationID='$vaccinationID'" ;
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+        $batchNo = $row['batchNo'];
+
+        $sql ="SELECT * FROM tb_batches WHERE batchNo ='$batchNo'" ;
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+        
+        $updatedQuantAv = $row['quantityAvailable'] -1;
+
+        $sql = "UPDATE tb_batches SET quantityAvailable='$updatedQuantAv' where batchNo='$batchNo';";
+        $result = mysqli_query($conn,$sql);
         echo '<script>alert("You have successfully confirmed the vaccination appointment");window.location.href = "vaccination.php";</script>';
         exit();
         
