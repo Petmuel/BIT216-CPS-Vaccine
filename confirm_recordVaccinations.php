3bcd7cd7ba961e2472f97626d635ffb8c638ec57
Petmuel <49142438+Petmuel@users.noreply.github.com>
2021-11-15 18:51:12 +0800

    l

--- confirm_recordVaccinations.php
+++ confirm_recordVaccinations.php
@@ -1,5 +1,6 @@
 <?php
     include_once 'db.php';
+    //Han Vui Ern Samuel B1700483
     if (isset($_GET['confirm'])) {
         //get vaccinationID from current vaccination
         $vaccinationID = $_GET['vID'];                     
@@ -10,4 +11,8 @@
         echo '<script>alert("You have successfully confirmed the vaccination appointment");window.location.href = "vaccination.php";</script>';
         exit();
         
+    }
+
+    if (isset($_GET['record'])) {
+
     }
