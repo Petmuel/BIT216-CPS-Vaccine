030bebb539311a33f7adb600e39f1ab597bec5aa
kelvin <fishaputrakelvin1810@gmail.com>
2021-11-18 21:53:01 +0800

    testing

--- requestVaccine.php
+++ requestVaccine.php
@@ -23,7 +23,7 @@
   <!-- jquery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-  <title>Welcome to CPS-Vaccine</title>
+  <title>Welcome to Govaccine</title>
 
   <style>
         body{
@@ -48,7 +48,7 @@
       <div class="container">
         <a class="navbar-brand" href="index.php">
             <img src="img/vaccinationIcon.png" width="45" height="auto" alt="PCVSIcon">
-            Cpsvaccine
+            Govaccine
           </a>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
